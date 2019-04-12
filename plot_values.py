0037fa3c271d98ecc8a022eeb7b430914ffb68fb
Bart Kastermans <kasterma@kasterma.net>
2019-04-12 14:10:58 +0200

    feat: new experimental plots

--- plot_values.py
+++ plot_values.py
@@ -1,7 +1,10 @@
+import time
+
 import matplotlib.pyplot as plt
 from collabcompet import NNAnalysis
 import numpy as np
 import pandas as pd
+import math
 
 x = NNAnalysis(20)
 dat = x.dats
@@ -48,3 +51,88 @@ for idx in range(N):
     xx = dat.loc[dat.net == 'actor_1_local'].loc[BASE + idx]
     axs[idx].plot(xx.episode_idx, xx.value)
 plt.show()
+
+datr = dat[prop]
+
+
+def in_class_simple(t0, epsilon, theta):
+    return in_class(t0//50, (t0 + 200)//50, (t0 + 400)//50, epsilon, theta)
+
+
+def in_class(t0, t1, t2, epsilon, theta):
+    def f(wt):
+        diff_during = math.fabs(wt[t1] - wt[t0])
+        diff_after = math.fabs(wt[t2] - wt[t1])
+        return diff_during > theta and diff_after < epsilon
+
+    return f
+
+
+def in_class_simple_t(t0):
+    return in_class_t(t0//50, (t0 + 200)//50, (t0 + 400)//50)
+
+
+def in_class_t(t0, t1, t2):
+    def f(wt):
+        diff_during = math.fabs(wt[t1] - wt[t0])
+        diff_after = math.fabs(wt[t2] - wt[t1])
+        return (diff_during, diff_after)
+
+    return f
+
+
+p = in_class_simple(1700, 1, 4)
+val_fn = in_class_t(1900//50, 2050//50, 2200//50)
+
+class TimeIt:
+    def __init__(self, id):
+        self.id = id
+
+    def __enter__(self):
+        self.start = time.monotonic()
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.end = time.monotonic()
+        print(f"Took {self.end - self.start}")
+
+
+with TimeIt("run1"):
+    x = [i for i in range(37500) if p(datr.loc[i, 'value'].values)]
+
+
+with TimeIt("run2"):
+    x = [val_fn(datr.loc[i, 'value'].values) for i in range(37500)]
+
+np.max(x, axis=0)
+np.mean(x, axis=0)
+np.min(x, axis=0)
+
+[(a,b) for a,b in x if a > 0.05 and b < 0.01]
+[i for i, x in enumerate(x) if x[0] == 0.0 and x[1] == 0.0]
+
+
+with TimeIt("test1") as timer:
+    [i for i in range(5000) if p(datr.loc[i, 'value'].values)]
+
+
+def from40(wt):
+    return math.fabs(wt[40]-wt[50])
+
+
+with TimeIt("2"):
+    w40_50 = [from40(datr.loc[i, 'value'].values) for i in range(37500)]
+
+
+engine = create_engine(f"sqlite:///{config['database_file']}", connect_args={'check_same_thread': False})
+Session = sessionmaker(bind=engine)
+session = Session()
+
+session.query(EpisodeScore).filter_by(run_id=20).all()
+[s.score for s in session.query(EpisodeScore).filter_by(run_id=20).all()]
+ss = [s.score for s in session.query(EpisodeScore).filter_by(run_id=20).all()]
+plt.plot(range(len(ss)), ss)
+plt.show()
+
+def running_mean(x, N):
+    cumsum = np.cumsum(np.insert(x, 0, 0))
+    return (cumsum[N:] - cumsum[:-N]) / float(N)
